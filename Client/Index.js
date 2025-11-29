6067e82e5f8aabb3c7404e4ca650e48db58b3bfe
piyush-200 <piyushadhikari740@gmail.com>
2025-11-29 18:02:21 +0530

    Update Index.js

--- Client/Index.js
+++ Client/Index.js
@@ -1,62 +1,14 @@
-// const express = require("express");
-// const app = express();
-// const userRoutes = require("./Route/User");
-// const profileRoutes = require("./Route/Profile");
-// const courseRoutes = require("./Route/Course");
-// const paymentRoutes = require("./Route/Payment");
-// const contactUsRoute = require("./Route/Contact");
-// const database = require("./Configuration/Database");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const { cloudinaryConnect } = require("./Configuration/Cloudinary");
-// const fileUpload = require("express-fileupload");
-// const dotenv = require("dotenv");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// const PORT = process.env.PORT || 4000;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-// dotenv.config();
-
-
-// database.connect();
- 
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-// 	cors({
-// 		origin: "*",
-// 		credentials: true,
-// 	})
-// );
-// app.use(
-// 	fileUpload({
-// 		useTempFiles: true,
-// 		tempFileDir: "/tmp/",
-// 	})
-// );
-
-
-// cloudinaryConnect();
-
-
-// app.use("/api/v1/auth", userRoutes);
-// app.use("/api/v1/profile", profileRoutes);
-// app.use("/api/v1/course", courseRoutes);
-// app.use("/api/v1/payment", paymentRoutes);
-// app.use("/api/v1/reach", contactUsRoute);
-
-
-// app.get("/", (req, res) => {
-// 	return res.json({
-// 		success: true,
-// 		message: "Welcome To StudyNotion",
-// 	});
-// });
-
-
-// app.listen(PORT, () => {
-// 	console.log(`App is listening at ${PORT}`);
-// });
 
 
